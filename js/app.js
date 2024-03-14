4aa63084d98382af881370a8dbf9cfb5ff291526
andrea-garofolo <garofolo.andrea@gmail.com>
2024-03-14 18:44:36 +0100

    formattazione corretta

--- js/app.js
+++ js/app.js
@@ -4,13 +4,14 @@
 for (var i = 0; i < 101; i++) {
     if (i % 15 == 0) {
         console.log("FizzBuzz")
+
     } else if (i % 3 == 0) {
-        console.log("Fizz");
+        console.log("Fizz")
 
     } else if (i % 5 == 0) {
-        console.log("Buzz");
-    }
-    else {
-        console.log("Fizz");
+        console.log("Buzz")
+
+    } else {
+        console.log("Fizz")
     }
 }
